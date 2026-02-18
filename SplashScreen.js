4e5a3832a91d2bd970603183def8295ccdf4eee8
Matheus Henrique <matheushenriqueowdail@gmail.com>
2026-02-18 00:14:45 -0300

    adding video SplashScreen

--- SplashScreen.js
+++ SplashScreen.js
@@ -1,41 +1,43 @@
 import { useNavigation } from "@react-navigation/native";
+import { Video, ResizeMode } from "expo-av";
 import React, { useEffect, useRef } from "react";
 import { StyleSheet, View, Animated, Image } from "react-native";
 
 export default function SplashScreen() {
   const navigation = useNavigation();
-  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const fadeAnim = useRef(new Animated.Value(1)).current;
+  const barAnim = useRef(new Animated.Value(0)).current;
+  const barWidth = barAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0%", "100%"],
+  });
+
   useEffect(() => {
-    Animated.timing(fadeAnim, {
+    Animated.timing(barAnim, {
       toValue: 1,
-      duration: 1500,
-      useNativeDriver: true,
-    }).start(() => {
-      Animated.loop(
-        Animated.sequence([
-          Animated.timing(fadeAnim, {
-            toValue: 1.1,
-            duration: 1000,
-            useNativeDriver: true,
-          }),
-          Animated.timing(fadeAnim, {
-            toValue: 1,
-            duration: 1000,
-            useNativeDriver: true,
-          }),
-        ]),
-      ).start();
-    });
-    setTimeout (() => {
-         navigation.navigate("Home");
-    }, 4000);
+      duration: 4000,
+      useNativeDriver: false,
+    }).start();
   }, []);
+
   return (
     <View style={styles.container}>
-      <Animated.Image
-        source={require("./assets/counters-icon.png")}
-        style={[styles.foto, { transform: [{ scale: fadeAnim }] }]}
+      <Video
+        source={require("./assets/LogoVideo916.mp4")}
+        style={styles.Video}
+        resizeMode={ResizeMode.COVER}
+        shouldPlay={true}
+        isMuted={true}
+        onPlaybackStatusUpdate={(status) => {
+          if (status.didJustFinish) {
+            navigation.navigate("Home");
+          }
+        }}
       />
+      {/*
+      <View style={styles.backBar}> 
+        <Animated.View style={[styles.loadingBar, {width: barWidth}]}/>
+      </View>*/}
     </View>
   );
 }
@@ -52,4 +54,30 @@ const styles = StyleSheet.create({
     width: 250,
     height: 250,
   },
+
+  Video: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+
+  },
+
+  backBar: {
+    width: '80%',
+    height: 10,
+    backgroundColor: '#333',
+    borderRadius: 5,
+    position: 'absolute',
+    bottom: 150,
+
+    overflow: 'hidden',
+  },
+
+  loadingBar: {
+    height: '100%',
+    backgroundColor: '#0087b1',
+  },
+
 });
